64d2b8b316f8f6ab9aa2a0bb4cc50f66ff905de4
rishit-exe <fizzvvvip@gmail.com>
2025-08-29 01:07:40 +0530

    url_fixx

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/portfolio/',          // <-- Add this line to set the base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
