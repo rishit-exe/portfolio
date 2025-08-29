90897bcb71f3fbf3fe666d14e6e7b1e8490e8e91
rishit-exe <rishit.vns05@gmail.com>
2025-08-29 23:21:12 +0530

    minor fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+  base: '/portfolio/',  // Base path for GitHub Pages deployment
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'src'),  // <-- Define the alias to resolve '@' to the 'src' folder
     },
   },
-}));
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+});
